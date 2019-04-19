ccaabd7c17c7075f282fce64561075f5dd70004c
rqcao <rqcao@outlook.com>
2019-04-19 16:23:25 +0800

    用async/await重写爬虫，能正常爬取

--- spider爬虫相关/new_spider.js
+++ spider爬虫相关/new_spider.js
@@ -1,71 +1,98 @@
 const axios = require('axios');
 const cheerio = require('cheerio')
 
-function spider() {
-  let spiderArr = generateFiveRandom(10);
-  let resArr = [];
-  let success = [];
-  let error = [];
-  for (let i = 0; i < spiderArr.length; i++) {
-    let res = getSingleArticle(spiderArr[i])
-    if(res.length === 0){
-      error.push(spiderArr[i])
-    }
-    success.push(spiderArr[i])
-    resArr.push(res);
+
+/**
+ * 重写的函数
+ */
+
+/**
+ * 控制时间
+ */
+const Thread = {
+  Sleep: (d) => {
+    //生成时间随机数
+    //1000-2000之间
+    let randomTime = parseInt(Math.random()*(2000-1000+1)+1000,10)
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve.call()
+      }, randomTime)
+    })
   }
-  console.log(resArr)
-  console.log(`success:${success}`)
 }
 
-//获取文章结构
-function getSingleArticle(id) {
-  return axios.get(`http://www.acfun.cn/a/ac${id}`)
-    .then((res) => {
-      let arr = [];
-      let html = res.data;
-      const $ = cheerio.load(html);
-      $('.article-content p').each(function (i, el) {
-        let content = $(el).text();
-        arr.push(content)
-        $(el).find('img').each(function (i, el) {
-          arr.push($(this).attr('src'))
-        })
-      })
-      $('.article-content div').each((i, el) => {
-        let content = $(el).text();
-        arr.push(content)
-        $(el).find('img').each(function (i, el) {
-          arr.push($(this).attr('src'))
-        })
-      })
-
-      arr = arr.filter((el, i) => {
-        return el !== ''
-      })
-      console.log(arr)
-      return arr;
-    })
-    .catch(e => {
-      return [];
-    })
-}
-
-// 4579559
-// 4581295
-//10119522
-// http://www.acfun.cn/a/ac4581295
-//getSingleArticle(10119522);
-//generateFiveRandom()
 //1.生成5个随机数，数值在500000-2000000之间
 function generateFiveRandom(count) {
   let arr = [];
   for (let i = 0; i < count; i++) {
-    let a = Math.round(Math.random() * 10000000 + Math.random() * 100000 + Math.random() * 100 + i)
+    let a = parseInt(Math.random()*(20000000-500000+1)+500000,10)
     arr.push(a)
   }
   console.log(arr)
   return arr;
 }
 
+async function spider(param) {
+  console.log(`正在爬取中`)
+  let spiderArr = generateFiveRandom(5)
+  let resArr = [];
+  let success = [];
+  let error = [];
+  for (let i = 0; i < spiderArr.length; i++) {
+    const url = `http://www.acfun.cn/a/ac${spiderArr[i]}`
+    let res = await getSingleArticle(url)
+    if (res.length === 0) {
+      error.push(spiderArr[i])
+    }else{
+      success.push(spiderArr[i])
+      resArr.push(res);
+    }
+    await Thread.Sleep(1000)
+  }
+  console.log(`成功：${success}`)
+  console.log(`失败：${error}`)
+  console.log(`结果：${resArr}`)
+  console.log('爬取完成')
+}
+
+
+async function getSingleArticle(url) {
+  try {
+    const res = await axios.get(url);
+    let arr = [];
+    let html = res.data;
+    const $ = cheerio.load(html);
+    $('.article-content p').each(function (i, el) {
+      let content = $(el).text();
+      arr.push(content)
+      $(el).find('img').each(function (i, el) {
+        arr.push($(this).attr('src'))
+      })
+    })
+    $('.article-content div').each((i, el) => {
+      let content = $(el).text();
+      arr.push(content)
+      $(el).find('img').each(function (i, el) {
+        arr.push($(this).attr('src'))
+      })
+    })
+
+    arr = arr.filter((el, i) => {
+      return el !== ''
+    })
+    console.log(arr)
+    return arr;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
 spider();
+// 4579559
+// 4581295
+//10119522
+// http://www.acfun.cn/a/ac4581295
+// spider();
+//5868513,1089310,2235871,4097122,4060463
